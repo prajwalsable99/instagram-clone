3139a798af44894165d9b21dc77e11c5fa8f3f5a
prajwal <prajsa99@gmail.com>
2022-10-27 17:50:49 +0530

    firebase and directory setup

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
+import firebaseContext from './context/firebase';
+
+import { firebase,fieldvalue } from './lib/firebase';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
+  <firebaseContext.Provider value={{firebase,fieldvalue}}>
     <App />
-  </React.StrictMode>
+  </firebaseContext.Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/lib/firebase.js
@@ -0,0 +1,26 @@
+import Firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+// import { seedDatabase } from '../seed';
+
+const config = {
+
+    apiKey: "AIzaSyByZ4I3I-aQmtYn5pbBiTEwL_4KPabLGh8",
+    authDomain: "instagram-dc81f.firebaseapp.com",
+    projectId: "instagram-dc81f",
+    storageBucket: "instagram-dc81f.appspot.com",
+    messagingSenderId: "804620268688",
+    appId: "1:804620268688:web:dedaf1af7f9a7011c51415"
+};
+
+const firebase = Firebase.initializeApp(config);
+
+const { fieldvalue } = Firebase.firestore;
+
+// seedDatabase(firebase);
+
+console.log("firebase",firebase);
+
+
+export { firebase, fieldvalue };
